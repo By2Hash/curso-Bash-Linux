02f800edd7acc4cabbe50df8ea3b2f8a532cf2ff
By2 <by2git2gmail.com>
2026-01-04 15:36:04 +0000

    Inclusion de un mensaje de GitHub

--- Conceptos/analizador.sh
+++ Conceptos/analizador.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# 1. CAPTURAR ARGUMENTO
-# Guardamos el primer texto que escribas después del script en esta variable
 LOG_FILE=$1
 
-# 2. VALIDACIÓN 1: ¿El usuario escribió algo?
-# -z devuelve "verdadero" si la variable está vacía
+
 if [ -z "$LOG_FILE" ]; then
     echo "❌ Error: Olvidaste indicar el archivo."
     echo "Ejecútalo así: ./analizador.sh server.log"
     exit 1  # 'exit 1' mata el script aquí. Ya no sigue bajando.
 fi
 
-# 3. VALIDACIÓN 2: ¿El archivo existe?
-# -f devuelve "verdadero" si es un archivo real
+
 if [ ! -f "$LOG_FILE" ]; then
     echo "❌ Error: El archivo '$LOG_FILE' no existe en esta carpeta."
     exit 1
@@ -39,3 +35,5 @@ echo "RESUMEN: Se detectaron $NUM_ATAQUES ataques."
 # 6. EXPORTAR REPORTE
 cat $LOG_FILE | grep "ERROR" > reporte_hackers.txt
 echo "✅ Reporte guardado en 'reporte_hackers.txt'"
+
+echo "Respaldo subido a GitHub"
